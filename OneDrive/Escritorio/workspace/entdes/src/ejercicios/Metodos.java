7bc5ae67b4064b409be35d7be075c8a4b38156bc
Gabriel-Jodra <gabrielito2008@live.com>
2021-03-23 23:58:59 +0100

    Nuevo commit después de Github 2

--- OneDrive/Escritorio/workspace/entdes/src/ejercicios/Metodos.java
+++ OneDrive/Escritorio/workspace/entdes/src/ejercicios/Metodos.java
@@ -2,6 +2,10 @@ package ejercicios;
 
 import java.util.Scanner;
 
+import java.util.Timer;
+
+import java.util.TimerTask;
+
 public class Metodos {
 
 	public static void main(String[] args) {
@@ -45,5 +49,24 @@ static int devuelveEnteroCorraspondiente(char caracter) {
 	
 }
 
+
+static void pideNumeroYDevuelveMultiplosDe3() {
+
+	Scanner scanner= new Scanner(System.in);
+
+	System.out.print("Introduce un número para calcular sus multiplos de 3 hasta el mismo= ");
+	int numero=scanner.nextInt();
+
+	if(numero<3) {
+		
+		System.out.println("No tiene multiplos de 3.");
+	} else {
+		
+		for (int i = 3; i <=numero; i+=3) {
+
+			System.out.println("Impresión de Multiplos= "+i);
+		}
 	}
+	}
+}
 
