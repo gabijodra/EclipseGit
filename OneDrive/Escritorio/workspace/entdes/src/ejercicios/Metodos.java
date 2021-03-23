dda8e0e2230fd774ef254aec5ac5ae64001358fe
Gabriel-Jodra <gabrielito2008@live.com>
2021-03-23 20:44:09 +0100

    Nuevo commit 2

--- OneDrive/Escritorio/workspace/entdes/src/ejercicios/Metodos.java
+++ OneDrive/Escritorio/workspace/entdes/src/ejercicios/Metodos.java
@@ -1,10 +1,26 @@
 package ejercicios;
 
+import java.util.Scanner;
+
 public class Metodos {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	}
+		static void devuelveStringsContatenados() {
+			
+			Scanner scanner =new Scanner(System.in);
+		
+				
+			System.out.println("Introduzca su primera cadena: ");
+			String entrada1=scanner.nextLine();
+			
+			
+			System.out.println("Introduzca su segunda cadena: ");
+			String entrada2=scanner.nextLine();
+			
+			System.out.println("Su texto es: " + entrada1 + entrada2);
+			
+		}
 
 	}
 
-}
